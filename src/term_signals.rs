6f3f1e434bb4b474ee9b09c25edd5db52e812c56
Xqhare <xqhare@web.de>
2026-03-11 21:00:00 +0100

    add(term_signals): common termination signal list

--- src/term_signals.rs
+++ src/term_signals.rs
@@ -1,5 +1,13 @@
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum TermSignal {
     SIGINT,
     SIGTERM,
     SIGQUIT,
 }
+
+/// A collection of signals that are commonly used to terminate a process.
+pub const TERM_SIGNALS: &[TermSignal] = &[
+    TermSignal::SIGINT,
+    TermSignal::SIGTERM,
+    TermSignal::SIGQUIT,
+];
